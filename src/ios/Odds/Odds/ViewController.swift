5bcfcb2784d3bec774f692be003e735b216c4d2f
Sean McQueen <sean@mcqueen.net>
2015-10-28 22:32:48 -0700

    Add empty swift project to the ios dir
    Adds empty iOS application in swift

---
+++ src/ios/Odds/Odds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Odds
+//
+//  Created by Sean McQueen on 10/28/15.
+//  Copyright (c) 2015 Sean McQueen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
